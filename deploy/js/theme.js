be2a181e5d3a0eb63a6bfa227e3b2c6d2b17f41d
pointwesthealthcare <pointwest.healthcare@gmail.com>
2016-06-02 11:59:51 +0800

    Add files via upload

---
+++ deploy/js/theme.js
@@ -0,0 +1,95 @@
+$(function () {
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top>30) {
+            $(".navbar-fixed-top").addClass("sticky");
+        }
+        else {
+            $(".navbar-fixed-top").removeClass("sticky");
+        }
+    });
+
+    // Flex
+    if ($(".flexslider").length) {
+        $('.flexslider').flexslider();
+    }
+
+    servicesOptions.initialize();
+
+    staticHeader.initialize();
+
+    portfolioItem.initialize();
+
+
+    // segun esto corrige el pedo del dropdown en tablets and such
+    // hay que testearlo!
+    $('.dropdown-toggle').click(function(e) {
+        e.preventDefault();
+        setTimeout($.proxy(function() {
+            if ('ontouchstart' in document.documentElement) {
+                $(this).siblings('.dropdown-backdrop').off().remove();
+            }
+        }, this), 0);
+    });
+});
+
+var portfolioItem = {
+    initialize: function () {
+        var $container = $("#portfolio_tem .left_box");
+        var $bigPics = $container.find(".big img");
+        var $thumbs = $container.find(".thumbs .thumb");
+
+        $bigPics.hide().eq(0).show();
+
+        $thumbs.click(function (e) {
+            e.preventDefault();
+            var index = $thumbs.index(this);
+            $bigPics.fadeOut();
+            $bigPics.eq(index).fadeIn();
+        });
+    }
+}
+
+var staticHeader = {
+    initialize: function () {
+        if ($(".navbar-static-top").length) {
+            $("body").css("padding-top", 0);
+        }
+    }
+}
+
+var servicesOptions = {
+    initialize: function () {
+        var $container = $(".services_circles");
+        var $texts = $container.find(".description .text");
+        var $circles = $container.find(".areas .circle");
+
+        $circles.click(function () {
+            var index = $circles.index(this);
+            $texts.fadeOut();
+            $texts.eq(index).fadeIn();
+            $circles.removeClass("active");
+            $(this).addClass("active");
+        });
+    }
+}
+
+
+$(document).ready(function(){
+    
+    $("#menuContent div").hide();
+    $("#menuContent div:first").show();
+    $("#subMenu li:first").addClass("active"); 
+    
+    $("#subMenu li a").click(function(){
+        $('#subMenu li').removeClass("active");
+        $(this).parent().addClass("active");
+        var current = $(this).attr("href");
+        $("#menuContent div:visible").fadeOut("fast");
+        $("#menuContent").animate({"height":$(current).height()},function(){
+            $(current).fadeIn("fast");
+        });
+       return false;
+    });
+    
+});
+
